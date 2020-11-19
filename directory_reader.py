30327c96c6ab03ecf8ee63deb570cfd4893a0d32
Manas Abhyankar <manasthemenace@gmail.com>
2020-11-19 13:12:49 -0800

    Added file to repo

---
+++ directory_reader.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os, sys, time
+
+def file_helper(directory_path, space_length):
+    p = Path(directory_path)
+    for x in p.iterdir():
+        if(x.is_file()):
+            print(' ' * space_length + '- ' + str(x).split('\\')[-1])
+            #print(int(time.time() - x.stat().st_atime))
+        elif(x.is_dir()):
+            print(' ' * space_length + '+ ' + str(x))
+            if(os.listdir(x)):
+                print(' ' * space_length + '|__')
+                file_helper(x, space_length + 4)
+
+
+if __name__ == "__main__":
+    cmd = sys.argv
+    if(Path(cmd[1]).exists()):
+        file_helper(Path(cmd[1]), 0)
+    else:
+        print("Invalid path!")
+    
