3eb03dba57a2a389b91fc1b73922c2336b4c2b56
Manas Abhyankar <manasthemenace@gmail.com>
2020-11-19 16:50:58 -0800

    Added some more readability

--- directory_reader.py
+++ directory_reader.py
@@ -5,8 +5,7 @@ def file_helper(directory_path, space_length):
     p = Path(directory_path)
     for x in p.iterdir():
         if(x.is_file()):
-            print(' ' * space_length + '- ' + str(x).split('\\')[-1])
-            #print(int(time.time() - x.stat().st_atime))
+            print((' ' * (space_length-1)) + '| ' + '- ' + str(x).split('\\')[-1])
         elif(x.is_dir()):
             print(' ' * space_length + '+ ' + str(x))
             if(os.listdir(x)):
